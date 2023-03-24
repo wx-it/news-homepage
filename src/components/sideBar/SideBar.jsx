f71571bd349fffb78338745d0c3da75e42e34c24
Lix <zinebbendjafer9@gmail.com>
2023-03-24 22:39:34 +0100

    desktop sidebar done

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -3,23 +3,29 @@ import "./sideBar.css";
 import { useState } from "react";
 
 const SideBar = ({ Logo, Menu, Close }) => {
+  const [toggle, setToggle] = useState(false);
 
-  const [toggle, setToggle] = useState(false)
-
-  const toggleMenu = ()=>{
-    setToggle(toggle => !toggle)
-  }
+  const toggleMenu = () => {
+    setToggle((toggle) => !toggle);
+  };
 
   return (
     <>
       <nav>
         <Logo />
+        <div className="desktop-links">
+          <a href="">Home</a>
+          <a href="">New</a>
+          <a href="">Popular</a>
+          <a href="">Trending</a>
+          <a href="">Categories</a>
+        </div>
         <button onClick={toggleMenu}>
           <Menu />
         </button>
       </nav>
       <div className={toggle ? "mobile-sideBar" : "mobile-sideBar-active"}>
-        <button className="close-btn" onClick={toggleMenu} >
+        <button className="close-btn" onClick={toggleMenu}>
           <Close />
         </button>
         <div className="links">
