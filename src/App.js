7f950eb1396f5ec4fc0d797b862cf4088f972766
Lix <zinebbendjafer9@gmail.com>
2023-03-24 21:56:52 +0100

    grid2 mobile done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import SideBar from "./components/sideBar/SideBar";
 import Grid1 from "./components/grid1/Grid1";
+import Grid2 from "./components/grid2/Grid2";
 import { ReactComponent as Logo } from "./assets/images/logo.svg";
 import { ReactComponent as Menu } from "./assets/images/icon-menu.svg";
 import { ReactComponent as Close } from "./assets/images/icon-menu-close.svg";
-import  mobileGrid1Image from "./assets/images/image-web-3-mobile.jpg";
-
+import mobileGrid1Image from "./assets/images/image-web-3-mobile.jpg";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       </header>
       <main>
         <Grid1 mobileGrid1Image={mobileGrid1Image} />
+        <Grid2 />
       </main>
     </>
   );
---
+++ src/components/grid2/Grid2.jsx
@@ -0,0 +1,29 @@
+import "./grid2.css";
+
+const Grid2 = () => {
+  return (
+    <div className="grid2">
+      <h2>New</h2>
+      <div>
+        <h3>Hydrogen VS Electric Cars</h3>
+        <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+      </div>
+      <div>
+        <h3>The Downsides of AI Artistry</h3>
+        <p>
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
+      </div>
+      <div>
+        <h3>Is VC Funding Drying Up?</h3>
+        <p>
+          Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Grid2;
